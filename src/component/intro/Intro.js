e34cd61e61a6b6607a330b196d4bc4e97b8bf88d
Kushan <krishnakushan7704@gmail.com>
2024-01-22 23:03:04 +0530

    added typewriter feature in about

--- src/component/intro/Intro.js
+++ src/component/intro/Intro.js
@@ -1,17 +1,25 @@
 import React from "react";
 import bg from '../../assets/image.png'
 import btnImg from '../../assets/hireme.png'
-import {Link} from 'react-scroll'
+import { Link } from 'react-scroll'
 import './intro.css'
+import { useTypewriter, Cursor } from 'react-simple-typewriter'
 
 const Intro = () => {
+    const [text] = useTypewriter({
+        words: ['Backend Developer', 'Frontend Developer', 'Full Stack Developer'],
+        loop: {},
+        typeSpeed: 120,
+        delaySpeed: 80,
+    });
+
     return (
         <section id="intro">
             <div className="introContent">
                 <span className="hello">Hello,</span>
-                <span className="introText">I'm <span className="introName">Kushan</span> <br /> Backend Developer</span>
-            <p className="introPara">I am a skilled and passionate web developer with experience in creating<br /> enterprise level backend application. </p>
-            <Link><button className="btn"><img src={btnImg} alt="" className="btnImg"/>Hire me</button></Link>
+                <span className="introText">I'm <span className="introName">Kushan <br /> </span> {text}<Cursor cursorColor="yellow" /> </span>
+                <p className="introPara">I am a skilled and passionate web developer with experience in creating<br /> enterprise level frontend and backend application. </p>
+                <Link><button className="btn"><img src={btnImg} alt="" className="btnImg" />Hire me</button></Link>
             </div>
             <img src={bg} alt="" className="bg" />
         </section>
