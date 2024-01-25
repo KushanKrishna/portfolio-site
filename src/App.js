b9e50c6dec6ef39695d0f6c833411a9dc162dfc4
Kushan <krishnakushan7704@gmail.com>
2024-01-26 00:25:27 +0530

    added new component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import Navbar from "./component/navbar/Navbar.js"
 import Intro  from "./component/intro/Intro.js";
 import Skills from "./component/skillls/Skills.js";
+import WhatIDo from "./component/about/WhatIDo.js";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
       <Skills />
+      <WhatIDo />
     </div>
   );
 }
---
+++ src/component/about/WhatIDo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const WhatIDo = () =>{
+
+    return(
+        <div>
+            What i do
+        </div>
+    )
+}
+
+export default WhatIDo;
