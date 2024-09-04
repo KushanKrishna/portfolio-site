6606d0ef0654bf9d403b86dc1d852de8353333df
Kushan <krishnakushan7704@gmail.com>
2024-09-04 13:19:09 +0530

    few adaptations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import Navbar from "./component/navbar/Navbar.js"
 import Intro  from "./component/intro/Intro.js";
 import Skills from "./component/skillls/Skills.js";
-import WhatIDo from "./component/about/WhatIDo.js";
 function App() {
   return (
     <div className="App">
       <Navbar />
       <Intro />
       <Skills />
-      <WhatIDo />
     </div>
   );
 }
